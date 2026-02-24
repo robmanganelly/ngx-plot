963a70e6cb1624c2e8183580a82ca7164e7d2cdf
Roberto Manganelly del Risco <rmangane@akamai.com>
2026-02-24 15:13:59 -0500

    feat: define app routes and create PlotPage component

--- apps/ngx-plot-demo/src/app/app.routes.ts
+++ apps/ngx-plot-demo/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [{
+    path: '',
+    loadComponent: () => import('./pages/plots').then(m => m.PlotPage)
+}];
---
+++ apps/ngx-plot-demo/src/app/pages/plots.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+    imports: [],
+    host: {},
+    providers: [],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    template: `hi`
+})
+export class PlotPage {}
